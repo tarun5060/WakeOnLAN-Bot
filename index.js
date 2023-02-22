d711899f7e82c70e32f25ee98f4c4a5ea2c45c5e
Tarun Rakesh <76225483+tarun5060@users.noreply.github.com>
2023-02-21 22:46:41 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+const config = require("./config.json");
+const discord = require("discord.js");
+const client = new discord.Client();
+const child_process = require("child_process");
+
+// Online status
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag} on ${Date()}!`);
+});
+
+// Wake command
+client.on("message", (msg) => {
+  if (msg.author.id === config.ownerID && msg.content === "wake") {
+    child_process.exec(
+      "wakeonlan -i ${config.ipAddress} ${config.macAddress}",
+      (err, stdout) => {
+        if (err) {
+          msg.channel.send(err);
+        } else {
+          msg.channel.send("woken ✅");
+        }
+      }
+    );
+  }
+});
+
+// Ping and uptime command
+client.on("message", (message) => {
+  if (message.content === "ping") {
+    const latency = Date.now() - message.createdTimestamp;
+    message.channel.send(
+      `🏓 Latency is \`${latency}ms\`. \nAPI Latency is \`${Math.round(
+        client.ws.ping
+      )}ms\`.`
+    );
+  } else if (message.content === "uptime") {
+    const hours = Math.round(client.uptime / 1000 / 3600);
+    message.channel.send(`Uptime is \`${hours}\` hours.`);
+  } else if (message.author.id === config.ownerID) {
+    if (message.content.startsWith("eval ")) {
+      evaluate(message);
+    } else if (message.content.startsWith("exec ")) {
+      execute(message);
+    } else if (message.content === "status") {
+      pingAmongUs(message);
+    }
+  }
+});
+
+// Evaluate command
+function evaluate(message) {
+  const code = message.content.slice("eval ".length);
+  try {
+    let evaled = eval(code);
+    if (typeof evaled !== "string") {
+      evaled = require("util").inspect(evaled);
+    }
+    message.channel.send(`\`\`\`\n${cleanText(evaled)}\`\`\``);
+  } catch (err) {
+    message.channel.send(`\`ERROR\` \`\`\`\n${cleanText(err)}\n\`\`\``);
+  }
+}
+
+// Execute command
+function execute(message) {
+  const command = message.content.slice("exec ".length);
+  child_process.exec(command, (err, stdout, stderr) => {
+    if (err) {
+      message.channel.send(err);
+    } else {
+      message.channel.send("```" + stdout + stderr + "```");
+    }
+  });
+}
+
+// Ping among us command
+function pingAmongUs(message) {
+  child_process.exec("ping ${config.ipAddress} -c 1", (err, stdout) => {
+    if (err) {
+      message.channel.send(err);
+    } else {
+      message.channel.send(stdout);
+    }
+  });
+}
+
+// Clean text for discord markdown
+function cleanText(text) {
+  if (typeof text === "string") {
+    return text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203));
+  }
+  return text;
+}
+
+client.login(config.token);
